d259e3cb9fa61622de232e62b1781b3a20d334f7
Grant Holland <gwholland3@gmail.com>
2021-01-26 18:38:36 -0800

    Use mongo to store users

--- sample_backend.py
+++ sample_backend.py
@@ -6,6 +6,8 @@ from flask_cors import CORS
 import random
 import string
 
+from model_mongodb import User
+
 app = Flask(__name__)
 CORS(app)
 
@@ -49,55 +51,49 @@ def hello_world():
 @app.route('/users', methods=['GET', 'POST'])
 def get_users():
     if request.method == 'GET':
-        if len(request.args) > 0:
-            subdict = {'users_list': []}
-            for user in users['users_list']:
-                if is_match(user):
-                    subdict['users_list'].append(user)
-            return subdict
-        return users
+        # search_username = request.args.get('name')
+        # search_job = request.args.get('job')
+        # if search_username and search_job:
+        #     return find_users_by_name_job(search_username, search_job)  # not converted to DB access yet
+        # elif search_username:
+        #     # return find_users_by_name(search_username) #old code left here for comparuson
+        #     users = User().find_by_name(search_username)
+        # else:
+        #     users = User().find_all()
+        users = User().find_by_queries(request.args)
+        return {"users_list": users}
     elif request.method == 'POST':
         user_to_add = request.get_json()
-        user_to_add['id'] = generate_random_id()
-        users['users_list'].append(user_to_add)
-        status = {'success': True, 'status_code': 201}
-        resp = jsonify(status, user_to_add)
+        # user_to_add['id'] = generate_random_id()
+        # users['users_list'].append(user_to_add)
+        # status = {'success': True, 'status_code': 201}
+        # resp = jsonify(status, user_to_add)
+        new_user = User(user_to_add)
+        new_user.save()
+        resp = jsonify(new_user), 201
         # 201 = content created
         return resp
-
-
-def generate_random_id():
-    id = ''.join(random.choice(string.ascii_lowercase) for _ in range(3))
-    id += ''.join(str(random.randint(0, 9)) for _ in range(3))
-    return id
-
-
-def is_match(user):
-    supported_filters = ['name', 'job']
-
-    for filter in request.args.keys():
-        if filter in supported_filters and user[filter] != request.args.get(filter):
-            return False
-    return True
 
 
 @app.route('/users/<id>', methods=['GET', 'DELETE'])
 def get_user(id):
     if request.method == 'GET':
         if id:
-            for user in users['users_list']:
-                if user['id'] == id:
-                    return user
-            return {}
-        return users
+            user = User({"_id": id})
+            if user.reload():
+                return user
+            else:
+                resp = jsonify(error="User not found"), 404
+                return resp
+        resp = jsonify(success=False), 400
+        return resp
     elif request.method == 'DELETE':
         if id:
-            for user in users['users_list']:
-                if user['id'] == id:
-                    users['users_list'].remove(user)
-                    resp = jsonify(success=True, status_code=204)
-                    return resp
-            resp = jsonify(success=False, status_code=404)
+            user = User({"_id": id})
+            if user.remove() > 0:
+                resp = jsonify(), 204
+                return resp
+            resp = jsonify(error="User not found"), 404
             return resp
-        resp = jsonify(success=False, status_code=400)
+        resp = jsonify(success=False), 400
         return resp
